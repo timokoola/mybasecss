68948e94f41adc1fa072e6589b6a1121a0824e3e
Timo Eemeli Koola <timoeemelikoola@MBP13.local>
2021-01-14 20:54:48 +0200

    rudimentaries

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-exports.default = () => {
-    console.log('Hello world');
-    return Promise.resolve();
-  };
-  
+const { dest, src, parallel, watch } = require('gulp');
+
+// Pull in each task
+const sass = require('./gulp-tasks/sass.js');
+const copy = () => {
+  return src('./src/html/*.html')
+    .pipe(dest('dist/'));
+}
+
+
+// Set each directory and contents that we want to watch and
+// assign the relevant task. `ignoreInitial` set to true will
+// prevent the task being run when we run `gulp watch`, but it
+// will run when a file changes.
+const watcher = () => {
+  watch('./src/scss/**/*.scss', { ignoreInitial: true }, sass);
+  watch('./src/html/**/*.html', { ignoreInitial: true }, copy);
+};
+
+// The default (if someone just runs `gulp`) is to run each task in parrallel
+exports.default = parallel(sass);
+
+// This is our watcher task that instructs gulp to watch directories and
+// act accordingly
+exports.watch = watcher;
